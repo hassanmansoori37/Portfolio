0edbfdf713724c085b1fd4c1694f4f7e3ae77da4
class12 <mansoorihassan409@gmail.com>
2026-05-26 16:55:58 +0500

    porotolio-completed

--- app.js
+++ app.js
@@ -6,3 +6,35 @@ function scrollToProjects(){
 
 }
 
+function toggleMenu(){
+
+    document.getElementById('navLinks').classList.toggle('show');
+
+}
+
+var typed = new Typed('#element', {
+
+   strings: [
+
+      'Frontend Developer',
+
+      'Frontend Developer | React Learner',
+
+      'Frontend Developer | React Learner | Learning MERN Stack',
+
+      'Frontend Developer | React Learner | Learning MERN Stack | Passionate About Web Development'
+
+   ],
+
+   typeSpeed: 40,
+
+   backSpeed: 10,
+
+   backDelay: 1000,
+
+   loop: true
+
+});
+
+
+
